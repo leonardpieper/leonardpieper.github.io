2adb49a9ff430cccb63c30ea49e97ee7cc0073a7
leonardpieper <leonard.pieper@outlook.com>
2016-01-16 17:58:37 +0100

    Test to see if deviceselection works

---
+++ vplan/getDevice.js
@@ -0,0 +1,25 @@
+/**
+ * Determine the mobile operating system.
+ * This function either returns 'iOS', 'Android' or 'unknown'
+ *
+ * @returns {String}
+ */
+function getDevice() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+  {
+    alert('iOS');
+	return 'iOS';
+  }
+  else if( userAgent.match( /Android/i ) )
+  {
+	alert('Android');
+    return 'Android';
+  }
+  else
+  {
+	alert('unknown');
+    return 'unknown';
+  }
+}
