566012d845fb943da333a03170aec92967fc7393
leonardpieper <leonard.pieper@outlook.com>
2017-06-24 14:42:57 +0200

    LuPO hinzugefügt

--- work/script/script.js
+++ work/script/script.js
@@ -24,3 +24,30 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+var slideIndexLuPO = 1;
+showSlidesLuPO();
+
+function plusSlidesLuPO(n) {
+  showSlidesLuPO(slideIndexLuPO += n);
+}
+
+function currentSlideLuPO(n) {
+  showSlidesLuPO(slideIndexLuPO = n);
+}
+
+function showSlidesLuPO(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlidesLuPO");
+  var dots = document.getElementsByClassName("dotLuPO");
+  if (n > slides.length) {slideIndexLuPO = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndexLuPO-1].style.display = "block";
+  dots[slideIndexLuPO-1].className += " active";
+}
